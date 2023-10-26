12c40e0bdf6d8d83e60853ede26d45117611e474
josewaihiga <josewaihiga@gmail.com>
2023-10-26 22:42:54 +1100

    Finished nav. Added some top-level styling. Installed firebase.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.components";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
-
+const Shop = () => {
+  return (<div><h1>Shop</h1></div>)
+}
 
 const App = () => {
   return (
     <Routes>
       <Route  path="/" element={<Navigation/>}>
         <Route index element={<Home />} />
-        <Route path="shop" />
-      
+        <Route path="shop" element={<Shop/>} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,19 +1,25 @@
 import { Link, Outlet } from "react-router-dom";
 
+import "./navigation.style.scss";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
 const Navigation = () => {
   return (
     <>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>Logo</div>
+          <CrwnLogo className="logo" />
         </Link>
 
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
         </div>
-        <h1>I am the navigation bar</h1>
       </div>
 
       <Outlet />
