73a2064a1d84485f61f0e9b22dab732be4d23d67
josewaihiga <josewaihiga@gmail.com>
2024-04-01 22:31:19 +1100

    Add Redux tooling
    Moved custom logger to module
    Stopped middleware from being used in prodcution environment
    Setup redux-devtools chrome extension

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.scss";
 
@@ -14,9 +15,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
