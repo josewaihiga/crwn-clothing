17e2163ee072d202e20ae6516f5e1adec8dee194
josewaihiga <josewaihiga@gmail.com>
2024-07-30 21:33:47 +1000

    Updated mobile product card layout to two columns

--- src/components/category-preview/category-preview.style.tsx
+++ src/components/category-preview/category-preview.style.tsx
@@ -17,4 +17,8 @@ export const Preview = styled.div`
       display: grid;
       grid-template-columns: repeat(auto-fit, minmax(min(19rem, 100%), 1fr));
       gap: 50px 20px;
+
+      @media (max-width: 600px){
+        grid-template-columns: repeat(auto-fit, minmax(min(10rem, 100%), 1fr));
+    }
 `
--- src/routes/category/category.styles.tsx
+++ src/routes/category/category.styles.tsx
@@ -6,6 +6,9 @@ export const Container = styled.div`
     grid-template-columns: repeat(auto-fit, minmax(min(19rem, 100%), 1fr));
     gap: 50px 20px;
 
+    @media (max-width: 600px){
+        grid-template-columns: repeat(auto-fit, minmax(min(10rem, 100%), 1fr));
+    }
 `
 
 export const Title = styled.h2`
